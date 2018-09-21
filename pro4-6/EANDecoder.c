10bd5fe4b11fa6354e40d3714f6c2f6655600899
BlockyG <maltegardo@gmail.com>
2018-09-21 11:30:11 +0200

    Finished addfrac2.c and started coding EANDecoder.c

---
+++ pro4-6/EANDecoder.c
@@ -0,0 +1,13 @@
+#import <stdio.h>
+
+int main(void)
+{
+    int n1, n2, n3, n4, n5, n6, n7, n8, n9, n10, n11, n12, n13;
+    int list[13] = {0,0,0,0,0,0,0,0,0,0,0,0,0};
+
+    printf("Input: ");
+    scanf("%1d", n1);
+    printf("Test: %1d", n1);
+  
+  return 0;
+}
---
+++ pro4-6/tempCodeRunnerFile.c
@@ -0,0 +1,10 @@
+
+int n1, n2, n3, n4, n5, n6, n7, n8, n9, n10, n11, n12, n13;
+int list[13] = {0,0,0,0,0,0,0,0,0,0,0,0,0};
+
+int main(void)
+{
+  scanf("1%d", n1);
+  printf("test: %d", n1);
+  
+  return 0;
