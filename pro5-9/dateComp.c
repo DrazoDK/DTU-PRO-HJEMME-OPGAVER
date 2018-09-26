03dbce8888e2615aebb3c92175e1144a5cc27925
BlockyG <maltegardo@gmail.com>
2018-09-26 13:47:46 +0200

    Finished first version of dateComp
    Might make a version that converts to num of days before doing comparissions

--- pro5-9/dateComp.c
+++ pro5-9/dateComp.c
@@ -2,29 +2,26 @@
 
 int main(void)
 {
-    int date1[3], date2[3], res, test;
+    int date1[3], date2[3], res = 4, segEq = 0, test;
 
     printf("Enter first date: ");
     scanf("%2d/%2d/%2d", &date1[0], &date1[1], &date1[2]);
     printf("Enter second date: ");
     scanf("%2d/%2d/%2d", &date2[0], &date2[1], &date2[2]);
 
-    for(int i = 2; i <= 2; i--){
+    for(int i = 2; i <= 2 && i >= 0; i--){
         if(date1[i] > date2[i]){res = 0;break;}
-        //if(date1[i] == date2[i]){res = 1;break;}
-        if(date1[i] < date2[i]){res = 2;break;}
+        if(date1[i] < date2[i]){res = 1;break;}
+        if(date1[i] == date2[i]){segEq++;}
     }
+    if(segEq == 3){res = 2;}
 
     switch(res){
-        case 0: printf("The date: %d/%d/%d is before %d/%d/%d", date2[0], date2[1], date2[2], date1[0], date1[1], date1[2]); break;
-        //case 1: printf("The dates: %d/%d/%d and %d/%d/%d are the same", date1[0], date1[1], date1[2], date2[0], date2[1], date2[2]); break;
-        case 2: printf("The date: %d/%d/%d is before %d/%d/%d", date1[0], date1[1], date1[2], date2[0], date2[1], date2[2]); break;
-        default:    printf("Illegal date"); break;
+        case 0: printf("The date: %d/%d/%d is before %d/%d/%ds\n", date2[0], date2[1], date2[2], date1[0], date1[1], date1[2]); break;
+        case 1: printf("The date: %d/%d/%d is before %d/%d/%dr\n", date1[0], date1[1], date1[2], date2[0], date2[1], date2[2]); break;
+        case 2: printf("The dates: %d/%d/%d and %d/%d/%d are the same\n", date1[0], date1[1], date1[2], date2[0], date2[1], date2[2]); break;
+        default:    printf("Illegal date\n"); break;
     }
-
-    //printf("%d/%d/%d", date1[0], date1[1], date1[2]);
-
-
 
   return 0;
 }
