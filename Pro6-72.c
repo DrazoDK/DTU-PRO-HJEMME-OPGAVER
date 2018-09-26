5482dd5e20c4dbc96260a726d052141e2146ce44
Emil Selbach Rasmussen <Emil.s.rasmussen@gmail.com>
2018-09-26 12:44:43 +0200

    coded program 6-7

---
+++ Pro6-72.c
@@ -0,0 +1,21 @@
+/* Prints a table of squares using an odd method */
+
+#import <stdio.h>
+
+int main(void)
+{
+    int  n, odd, square;
+
+    printf("This program prints a table of squares.\n");
+    printf("Enter number of entries in table: ");
+    scanf("%d", &n);
+
+
+    odd = 3;
+        for (square = 1, int i = 1; i <= n; odd += 2, ++i){
+            printf("%10d%10d\n", i, square);
+            
+            square += odd;
+        }
+    return 0;
+    }
