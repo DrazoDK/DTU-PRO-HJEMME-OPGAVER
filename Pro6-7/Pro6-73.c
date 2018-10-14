a35724518587d65727af2c0a1bf9b1c91e2b5aa6
BlockyG <maltegardo@gmail.com>
2018-10-14 16:12:47 +0200

    Added Pro6-73

---
+++ Pro6-7/Pro6-73.c
@@ -0,0 +1,17 @@
+/* Prints a table of squares using an odd method */
+
+#import <stdio.h>
+
+int main(void)
+{
+    int n;
+
+    printf("This program prints a table of squares.\n");
+    printf("Enter number of entries in table: ");
+    scanf("%d", &n);
+
+        for (int square = 1, i = 1, odd = 3; i <= n; square += odd, ++i, odd += 2){
+            printf("%10d%10d\n", i, square);
+        }
+    return 0;
+    }
